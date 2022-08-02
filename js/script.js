f19c041ba8ed52b71e9e99d23bde342bf12527d4
Lucas Darwich <lucasdarwich@gmail.com>
2022-08-02 12:33:33 -0300

    funcion flecha

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 let operacion = prompt("Indicar tipo de operación (USD para convertir a Dolares - ARS para convertir a Pesos)").toUpperCase();
 
+//Funcion Flecha
+const montoDolares = (montoPesos, tasaCambio) => montoPesos / tasaCambio;
+const montoPesos = (montoDolares, tasaCambio) => montoDolares * tasaCambio;
+
 if (operacion == "USD") {
     let pesos = parseFloat(prompt("Ingresar el Monto en pesos "));
     let tc = parseFloat(prompt("Ingresar Tasa de Cambio"));
-    montoDolares(pesos, tc);
+    let resultado = montoDolares(pesos, tc);
+    console.log(`El monto ingresado: ${pesos} equivale a USD: ${resultado}`); 
+
 } else if (operacion == "ARS") {
     let dolares = parseFloat(prompt("Ingresar el Monto en Dolares "));
     let tc = parseFloat(prompt("Ingresar Tasa de Cambio"));
-    montoPesos(dolares, tc);
+    let resultado = montoPesos(dolares, tc);
+    console.log(`El monto ingresado: ${dolares} equivale a ARS: ${resultado}`);
 } else {
     alert("Opción incorrecta. Intentar nuevamente");
 }
 
-// función para convertir de ARS a USD
+
+
+// Funciones (se hicieron primero para luego pasarlas a funciones flecha)
+
+/* //función para convertir de ARS a USD
 function montoDolares(montoPesos, tasaCambio) {
     let dolares = montoPesos / tasaCambio;
     console.log("El monto ingresado ($ " + montoPesos + ") es igual a " + dolares + " USD");
-}
+} */ 
 
-//función para convertir de USD a ARS
+/* //función para convertir de USD a ARS
 function montoPesos(montoDolares, tasaCambio) {
     let pesos = montoDolares * tasaCambio;
     console.log("El monto ingresado (USD " + montoDolares + ") es igual a " + pesos + " ARS");
-}
+}  */
